05e9da4aa2082d56c1526ad0aef7e9b4ee07dda1
Kauan Rodrigues Lima <162514268+Kauanrodrigues01@users.noreply.github.com>
2025-03-24 10:26:34 -0300

    Update urls.py

--- selections/urls.py
+++ selections/urls.py
@@ -3,5 +3,5 @@ from .views import download_csv_tier_report, download_excel_tier_report
 
 urlpatterns = [
     path('generate-report/csv/<str:tier_code>/', download_csv_tier_report, name='download_csv_tier_report'),
-    path('generate-report/excel/<str:tier_code>/', download_excel_tier_report, name='download_excel_tier_report'),
+    path('generate-report/excel/', download_excel_tier_report, name='download_excel_tier_report'),
 ]
